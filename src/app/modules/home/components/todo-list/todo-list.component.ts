03dec447415ae89af297dc0eaa5d87189db4c14c
joota <jrtatay@hotmail.com>
2023-05-27 10:45:01 -0300

    adicionando botão e background

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Model } from '../../model';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent {
+
+  public taskList: Array<Model> = [
+    { task: "Minha Nova task", checked: true},
+    { task: "Minha Nova task 2", checked: true}
+  ]
+public deleteItemTaskList(event:number){
+  this.taskList.splice(event, 1)
+}
+
+public deleteAllTask(){
+  const confirm = window.confirm('Você tem certeza que deseja deletar tudo?')
+  if(confirm){
+    this.taskList = [];
+  }
+}
+
 
 }
---
+++ src/app/modules/home/model.ts
@@ -0,0 +1,4 @@
+export interface Model {
+  task: string,
+  checked:boolean
+}
